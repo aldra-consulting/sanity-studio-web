c04dffac62e558d52ed9cf5007082fb0c64fc666
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-03-06 09:03:05 +0100

    chore: add common types

---
+++ src/types/common.d.ts
@@ -0,0 +1,12 @@
+export type Primitive =
+  | string
+  | number
+  | bigint
+  | boolean
+  | null
+  | symbol
+  | undefined;
+
+export type Hashable = NonNullable<Primitive>;
+
+export type Nullable<T = never> = T | null | undefined;
