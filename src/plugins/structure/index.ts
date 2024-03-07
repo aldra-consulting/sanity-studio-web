899cf4156adbb4770f93af5c31e51a93a9eb3b2e
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-03-07 08:38:10 +0100

    feat: add custom content structure

--- src/plugins/structure/index.ts
+++ src/plugins/structure/index.ts
@@ -1,4 +1,37 @@
 import { type PluginOptions } from 'sanity';
 import { structureTool } from 'sanity/structure';
 
-export default () => structureTool() as PluginOptions;
+export default () =>
+  structureTool({
+    structure: (builder) =>
+      builder
+        .list()
+        .title('Content')
+        .items([
+          ...builder
+            .documentTypeListItems()
+            .filter(
+              (listItem) => !['language'].includes(listItem.getId() ?? '')
+            )
+            .map((listItem) => listItem.showIcon(false)),
+          builder.divider(),
+          builder
+            .listItem()
+            .title('Settings')
+            .showIcon(false)
+            .child(
+              builder
+                .list()
+                .title('Settings')
+                .showIcons(false)
+                .items([
+                  builder
+                    .listItem()
+                    .title('Languages')
+                    .child(
+                      builder.documentTypeList('language').title('Languages')
+                    ),
+                ])
+            ),
+        ]),
+  }) as PluginOptions;
