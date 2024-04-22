0179aa0c283bdcbd9416c3f13d3ab064f9fb5fe8
Alexander Zakharov <alexander.zakharov@aldra.no>
2024-04-22 15:23:56 +0200

    feat: disable document actions on mission bookmarks documents

--- src/document/index.ts
+++ src/document/index.ts
@@ -1,6 +1,13 @@
 import { type DocumentPluginOptions } from 'sanity';
 
 export default {
+  actions: (previous, { schemaType }) => {
+    if (schemaType === 'bookmark.missions') {
+      return [];
+    }
+
+    return previous;
+  },
   newDocumentOptions: (previous, { creationContext: { schemaType } }) => {
     if (schemaType === 'bookmark.missions') {
       return [];
